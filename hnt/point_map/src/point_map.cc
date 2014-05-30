29fe1ba612a5a5406760f43736da8accf27b8e68
David Feil-Seifer <dave@cse.unr.edu>
2014-05-30 01:59:24 -0700

    point_map: code cleanup

--- hnt/point_map/src/point_map.cc
+++ hnt/point_map/src/point_map.cc
@@ -1,3 +1,33 @@
+/*
+ * point_map
+ * Copyright (c) 2014, David Feil-Seifer, University of Nevada, Reno
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the University of Nevada, Reno nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <ros/ros.h>
 #include <yaml-cpp/yaml.h>
 #include <point_map/PointMap.h>
@@ -20,17 +50,6 @@ struct Point2D
 {
   _T x;
   _T y;
-  
-  Point2D()
-  {
-    //
-  }
-  
-  Point2D( _T p_x, _T p_y )
-  {
-    x = p_x;
-    y = p_y;
-  }
 };
 
 struct Segment
@@ -70,9 +89,7 @@ struct Segment
         }
       }
       else
-      {
-//        int dir = rise / fabs( rise );
-      
+      {     
         for( int j = v0; j != vn ; j+= rise / fabs( rise) )
         {
           int v = j;
@@ -182,21 +199,6 @@ int main( int argc, char* argv[] )
     ROS_WARN("You must provide a map file to read from");
     return 1;
   }
-  
-  /*
-  std::ifstream fin(argv[1]);
-  
-  if(!fin.good())
-  {
-    ROS_WARN("File not found: [%s]", argv[1]);
-    return 1;
-  }
-  
-  YAML::Parser parser(fin);
-  YAML::Node doc;
-  parser.GetNextDocument(doc);
-
-  */
 
   YAML::Node doc = YAML::LoadFile(argv[1]);
   printf( "num segments: %d\n", doc.size() );
