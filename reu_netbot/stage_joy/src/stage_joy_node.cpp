3c38accd3493785b06c4597f54e82a5b6344f4de
davefeilseifer <david.feilseifer@gmail.com>
2014-06-10 23:46:52 -0700

    stage_joy: * changed rotation axis to match other functionality * added debug message to check stage_joy_node output * removed unnecessary file

--- reu_netbot/stage_joy/src/stage_joy_node.cpp
+++ reu_netbot/stage_joy/src/stage_joy_node.cpp
@@ -26,8 +26,8 @@ StageBot::StageBot():
 {
 	nh_.param("axis_linear", linear_, linear_);
 	nh_.param("axis_angular", angular_, angular_);
-	nh_.param("scale_angular", a_scale_, a_scale_);
-	nh_.param("scale_linear", l_scale_, l_scale_);
+	nh_.param("scale_angular", a_scale_, 0.5);
+	nh_.param("scale_linear", l_scale_, 0.6);
 
 	vel_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
 
@@ -38,11 +38,12 @@ void StageBot::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
 	geometry_msgs::Twist vel;
 	if ( joy->buttons[4] == 1 && joy->buttons[5] == 1)
-		{
-		vel.angular.z = linear_*joy->axes[3];
-		vel.linear.x = angular_*joy->axes[1];
-		}
-	vel_pub_.publish(vel);
+  {
+    vel.angular.z = l_scale_*joy->axes[2];
+    vel.linear.x = a_scale_*joy->axes[1];
+    ROS_DEBUG( "xaxis: %f(%f), zrot: %f(%f)", joy->axes[1], vel.linear.x, joy->axes[2], vel.angular.z );
+  }
+  vel_pub_.publish(vel);
 }
 
 
