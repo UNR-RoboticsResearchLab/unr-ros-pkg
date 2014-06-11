bfd71c85820d638aa9bc6cf6a5a85c8cbae368cd
davefeilseifer <david.feilseifer@gmail.com>
2014-06-10 23:59:42 -0700

    stage_joy: * modified stage_joy to re-send messages at constant rate while deadman buttons are pushed

--- reu_netbot/stage_joy/src/stage_joy_node.cpp
+++ reu_netbot/stage_joy/src/stage_joy_node.cpp
@@ -6,51 +6,77 @@ class StageBot
 {
 public:
 	StageBot();
+	void spin();
 
 private:
 	void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
-	
+
 	ros::NodeHandle nh_;
 
 	int linear_, angular_;
 	double l_scale_, a_scale_;
 	ros::Publisher vel_pub_;
 	ros::Subscriber joy_sub_;
-	
-};
 
+  double pub_rate_;
+  bool deadman_;
+	geometry_msgs::Twist vel_;
+};
 
 StageBot::StageBot():
 	linear_(1),
-	angular_(2)
+	angular_(2),
+  deadman_(false)
 {
 	nh_.param("axis_linear", linear_, linear_);
 	nh_.param("axis_angular", angular_, angular_);
 	nh_.param("scale_angular", a_scale_, 0.5);
 	nh_.param("scale_linear", l_scale_, 0.6);
-
+  nh_.param("pub_rate", pub_rate_, 10.0);
 	vel_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
 
 	joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 100, &StageBot::joyCallback, this);
+
+  vel_.linear.x = 0.0;
+  vel_.angular.z = 0.0;
 }
 
 void StageBot::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
-	geometry_msgs::Twist vel;
+  // joy_node only sends messages when changed, so need to store vels in class variables in order to publish intermittently
+
 	if ( joy->buttons[4] == 1 && joy->buttons[5] == 1)
   {
-    vel.angular.z = l_scale_*joy->axes[2];
-    vel.linear.x = a_scale_*joy->axes[1];
-    ROS_DEBUG( "xaxis: %f(%f), zrot: %f(%f)", joy->axes[1], vel.linear.x, joy->axes[2], vel.angular.z );
+    deadman_ = true;
   }
-  vel_pub_.publish(vel);
+  else
+  {
+    deadman_ = false;
+  }
+  vel_.angular.z = l_scale_*joy->axes[angular_];
+  vel_.linear.x = a_scale_*joy->axes[linear_];
+  ROS_DEBUG( "xaxis: %f(%f), zrot: %f(%f)", joy->axes[linear_], vel_.linear.x, joy->axes[angular_], vel_.angular.z );
+
 }
 
+void StageBot::spin()
+{
+  ros::Rate loop_rate(pub_rate_);
+
+  while( ros::ok() )
+  {
+    if( deadman_ )
+    {
+      vel_pub_.publish(vel_);
+    }
+    loop_rate.sleep();
+    ros::spinOnce();
+  }
+}
 
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "joystick");
 	StageBot stage_bot;
-
-	ros::spin();
+  stage_bot.spin();
 }
